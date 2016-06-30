36d50aea89b162c9fbde5c56b0046d4a17ec7b1b
jack <jack@nelsoncash.com>
2016-06-30 15:57:22 -0500

    update algo

--- trainer/main.py
+++ trainer/main.py
@@ -19,12 +19,38 @@ def get_all_shots():
 
 def init_tensorflow():
     filenames = tf.train.string_input_producer(ALL_FILENAMES)
-    reader = tf.WholeFileReader()
-    key, value = reader.read(filenames)
+    # reader = tf.WholeFileReader()
+    # key, value = reader.read(filenames)
+    init_op = tf.initialize_all_variables()
+    # with tf.Session() as sess:
+    #   sess.run(init_op)
+    images = tf.convert_to_tensor(ALL_FILENAMES)
+    labels = tf.convert_to_tensor(ALL_VECTORS)
+# x = tf.placeholder(tf.float32, [None, 30000])
+    input_queue = tf.train.slice_input_producer([images, labels])
+    print(input_queue)
+
+    #image, label = read_images_from_disk(input_queue)
+
     init_op = tf.initialize_all_variables()
     with tf.Session() as sess:
       sess.run(init_op)
-# x = tf.placeholder(tf.float32, [None, 30000])
+      tf.train.start_queue_runners(sess=sess)
+    #   sess.run([images[0], labels[0]])
+
+def read_images_from_disk(input_queue):
+    """Consumes a single filename and label as a ' '-delimited string.
+    Args:
+      filename_and_label_tensor: A scalar string tensor.
+    Returns:
+      Two tensors: the decoded image, and the string label.
+    """
+    label = input_queue[1]
+    file_contents = tf.read_file(input_queue[0])
+    print(file_contents)
+    example = tf.image.decode_png(file_contents, channels=3)
+    print(example)
+    return example, label
 
 if __name__ == '__main__':
     get_all_shots()
