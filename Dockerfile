7349ab753d800d4492b3f9320292b6d5171043b8
jack <jack@nelsoncash.com>
2016-07-06 17:19:33 -0500

    add docker support

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM gcr.io/tensorflow/tensorflow:latest-devel
+
+RUN apt-get update
+
+ADD . /app
+
+EXPOSE 8000
+
+VOLUME [".:/app"]
+
+WORKDIR /app
+
+CMD ["/bin/bash"]
--- trainer/main.py
+++ trainer/main.py
@@ -1,5 +1,21 @@
 import tensorflow as tf
+from tensorflow.models.image.cifar10 import cifar10
 import json
+
+# Basic model parameters as external flags.
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+flags.DEFINE_integer('num_epochs', 2, 'Number of epochs to run trainer.')
+flags.DEFINE_integer('hidden1', 128, 'Number of units in hidden layer 1.')
+flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
+# flags.DEFINE_integer('batch_size', 100, 'Batch size.')
+
+
+IMAGE_WIDTH = 200
+IMAGE_HEIGHT = 150
+IMAGE_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT
+BATCH_SIZE = 100
 
 ALL_SHOTS = []
 ALL_VECTORS = []
@@ -13,36 +29,108 @@ def get_all_shots():
             ALL_SHOTS.append(v)
             ALL_VECTORS.append(v['tagVector'])
             ALL_IDS.append(v['id'])
-            ALL_FILENAMES.append("".join(("../tmp-bw/", k, ".png")))
+            ALL_FILENAMES.append("".join(("../tmp-cifar/", k, ".bin")))
         print(ALL_VECTORS)
         print(ALL_FILENAMES)
 
-def init_tensorflow():
-    filenames = tf.train.string_input_producer(ALL_FILENAMES)
-    # reader = tf.WholeFileReader()
-    # key, value = reader.read(filenames)
-    images = tf.convert_to_tensor(ALL_FILENAMES)
-    labels = tf.convert_to_tensor(ALL_VECTORS)
-    init_op = tf.initialize_all_variables()
-    with tf.Session() as sess:
-      sess.run(init_op)
-    print(labels.shape[0])
-    print(images)
-# x = tf.placeholder(tf.float32, [None, 30000])
-    # input_queue = tf.train.slice_input_producer([images, labels])
-    # print(input_queue)
-    #
-    # #image, label = read_images_from_disk(input_queue)
-    #
-    # init_op = tf.initialize_all_variables()
-    # graph = tf.get_default_graph()
-    # for op in  graph.get_operations():
-    #     print(op.get_attr())
-    # print(operations)
-    # with tf.Session() as sess:
-    #   sess.run(init_op)
-    #   tf.train.start_queue_runners(sess=sess)
-    #   sess.run([images[0], labels[0]])
+def run_training():
+    """Train MNIST for a number of steps."""
+
+    # Tell TensorFlow that the model will be built into the default Graph.
+    with tf.Graph().as_default():
+        global_step = tf.Variable(0, trainable=False)
+        # Input images and labels.
+        images, labels = inputs(train=True, batch_size=BATCH_SIZE,
+                                num_epochs=FLAGS.num_epochs)
+
+        logits = cifar10.inference(images)
+
+        # Calculate loss.
+        loss = cifar10.loss(logits, labels)
+        print(loss)
+
+        # Build a Graph that trains the model with one batch of examples and
+        # updates the model parameters.
+        train_op = cifar10.train(loss, global_step)
+
+        # Create a saver.
+        saver = tf.train.Saver(tf.all_variables())
+
+        # Build the summary operation based on the TF collection of Summaries.
+        summary_op = tf.merge_all_summaries()
+
+        # Build an initialization operation to run below.
+        init = tf.initialize_all_variables()
+
+        # Start running operations on the Graph.
+        sess = tf.Session(config=tf.ConfigProto(
+            log_device_placement=FLAGS.log_device_placement))
+        sess.run(init)
+
+        # Start the queue runners.
+        tf.train.start_queue_runners(sess=sess)
+
+        summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph)
+
+        for step in xrange(FLAGS.max_steps):
+          start_time = time.time()
+          _, loss_value = sess.run([train_op, loss])
+          duration = time.time() - start_time
+
+          assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+          if step % 10 == 0:
+            num_examples_per_step = BATCH_SIZE
+            examples_per_sec = num_examples_per_step / duration
+            sec_per_batch = float(duration)
+
+            format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                          'sec/batch)')
+            print (format_str % (datetime.now(), step, loss_value,
+                                 examples_per_sec, sec_per_batch))
+
+          if step % 100 == 0:
+            summary_str = sess.run(summary_op)
+            summary_writer.add_summary(summary_str, step)
+
+          # Save the model checkpoint periodically.
+          if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+            checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+            saver.save(sess, checkpoint_path, global_step=step)
+
+def inputs(train, batch_size, num_epochs):
+    if not num_epochs: num_epochs = None
+    with tf.name_scope('input'):
+        filename_queue = tf.train.string_input_producer(
+            ALL_FILENAMES, num_epochs=num_epochs)
+
+        # Even when reading in multiple threads, share the filename
+        # queue.
+        read_input = read_cifar10(filename_queue)
+        reshaped_image = tf.cast(read_input.uint8image, tf.float32)
+
+        height = IMAGE_HEIGHT
+        width = IMAGE_WIDTH
+
+        # Image processing for evaluation.
+        # Crop the central [height, width] of the image.
+        resized_image = tf.image.resize_image_with_crop_or_pad(reshaped_image,
+                                                             width, height)
+
+        # Subtract off the mean and divide by the variance of the pixels.
+        float_image = tf.image.per_image_whitening(resized_image)
+        #image, label = read_and_decode(filename_queue)
+
+        # Shuffle the examples and collect them into batch_size batches.
+        # (Internally uses a RandomShuffleQueue.)
+        # We run this in two threads to avoid being a bottleneck.
+        images, sparse_labels = tf.train.shuffle_batch(
+            [float_image, read_input.label], batch_size=batch_size, num_threads=2,
+            capacity=1000 + 3 * batch_size,
+            # Ensures a minimum amount of shuffling of examples.
+            min_after_dequeue=1000)
+
+        return images, sparse_labels
 
 def read_and_decode(filename_queue):
     reader = tf.TFRecordReader()
@@ -59,7 +147,7 @@ def read_and_decode(filename_queue):
     # length mnist.IMAGE_PIXELS) to a uint8 tensor with shape
     # [mnist.IMAGE_PIXELS].
     image = tf.decode_raw(features['image_raw'], tf.uint8)
-    image.set_shape([mnist.IMAGE_PIXELS])
+    image.set_shape([IMAGE_SIZE])
 
     # OPTIONAL: Could reshape into a 28x28 image and apply distortions
     # here.  Since we are not applying any distortions in this
@@ -74,39 +162,64 @@ def read_and_decode(filename_queue):
 
     return image, label
 
-def inputs(num_epochs):
-    filename_queue = tf.train.string_input_producer(
-        ALL_FILENAMES, num_epochs=num_epochs)
 
-    # Even when reading in multiple threads, share the filename
-    # queue.
-    image, label = read_and_decode(filename_queue)
-
-    # Shuffle the examples and collect them into batch_size batches.
-    # (Internally uses a RandomShuffleQueue.)
-    # We run this in two threads to avoid being a bottleneck.
-    images, sparse_labels = tf.train.shuffle_batch(
-        [image, label], batch_size=batch_size, num_threads=2,
-        capacity=1000 + 3 * batch_size,
-        # Ensures a minimum amount of shuffling of examples.
-        min_after_dequeue=1000)
-
-    return images, sparse_labels
-
-def read_images_from_disk(input_queue):
-    """Consumes a single filename and label as a ' '-delimited string.
+def read_cifar10(filename_queue):
+    """Reads and parses examples from CIFAR10 data files.
+    Recommendation: if you want N-way read parallelism, call this function
+    N times.  This will give you N independent Readers reading different
+    files & positions within those files, which will give better mixing of
+    examples.
     Args:
-      filename_and_label_tensor: A scalar string tensor.
+    filename_queue: A queue of strings with the filenames to read from.
     Returns:
-      Two tensors: the decoded image, and the string label.
+    An object representing a single example, with the following fields:
+      height: number of rows in the result (32)
+      width: number of columns in the result (32)
+      depth: number of color channels in the result (3)
+      key: a scalar string Tensor describing the filename & record number
+        for this example.
+      label: an int32 Tensor with the label in the range 0..9.
+      uint8image: a [height, width, depth] uint8 Tensor with the image data
     """
-    label = input_queue[1]
-    file_contents = tf.read_file(input_queue[0])
-    print(file_contents)
-    example = tf.image.decode_png(file_contents, channels=3)
-    print(example)
-    return example, label
+
+    class CIFAR10Record(object):
+        pass
+    result = CIFAR10Record()
+
+    # Dimensions of the images in the CIFAR-10 dataset.
+    # See http://www.cs.toronto.edu/~kriz/cifar.html for a description of the
+    # input format.
+    label_bytes = 1  # 2 for CIFAR-100
+    result.height = IMAGE_HEIGHT
+    result.width = IMAGE_WIDTH
+    result.depth = 3
+    image_bytes = result.height * result.width * result.depth
+    # Every record consists of a label followed by the image, with a
+    # fixed number of bytes for each.
+    record_bytes = label_bytes + image_bytes
+
+    # Read a record, getting filenames from the filename_queue.  No
+    # header or footer in the CIFAR-10 format, so we leave header_bytes
+    # and footer_bytes at their default of 0.
+    reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+    result.key, value = reader.read(filename_queue)
+
+    # Convert from a string to a vector of uint8 that is record_bytes long.
+    record_bytes = tf.decode_raw(value, tf.uint8)
+
+    # The first bytes represent the label, which we convert from uint8->int32.
+    result.label = tf.cast(
+      tf.slice(record_bytes, [0], [label_bytes]), tf.int32)
+
+    # The remaining bytes after the label represent the image, which we reshape
+    # from [depth * height * width] to [depth, height, width].
+    depth_major = tf.reshape(tf.slice(record_bytes, [label_bytes], [image_bytes]),
+                           [result.depth, result.height, result.width])
+    # Convert from [depth, height, width] to [height, width, depth].
+    result.uint8image = tf.transpose(depth_major, [1, 2, 0])
+
+    return result
 
 if __name__ == '__main__':
     get_all_shots()
-    init_tensorflow()
+    run_training()
